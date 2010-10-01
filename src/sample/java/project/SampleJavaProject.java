059a26260610ee2878e4929ec3adcc880b43c313
Christopher Wellons <mosquitopsu@gmail.com>
2010-10-01 00:05:12 -0400

    Initial commit of basic structure.

---
+++ src/sample/java/project/SampleJavaProject.java
@@ -0,0 +1,11 @@
+package sample.java.project;
+
+public class SampleJavaProject {
+    public static void main(String[] args) {
+	System.out.println("Hello world!");
+    }
+
+    public int add(int a, int b) {
+	return a + b;
+    }
+}
---
+++ test/sample/java/project/SampleJavaProjectTest.java
@@ -0,0 +1,19 @@
+package sample.java.project;
+
+import junit.framework.TestCase;
+
+public class SampleJavaProjectTest extends TestCase {
+    public SampleJavaProjectTest(String name) {
+	super(name);
+    }
+
+    SampleJavaProject sjp;
+
+    public void setUp() {
+	sjp = new SampleJavaProject();
+    }
+
+    public void testAdd() {
+	assert(sjp.add(3, 4) == 7);
+    }
+}
