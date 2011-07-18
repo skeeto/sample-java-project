50297d7a9c30f6fbe5654881d5f0c5248f6afc9b
Christopher Wellons <mosquitopsu@gmail.com>
2011-07-18 15:08:20 -0400

    Make use of the new library.

--- src/sample/java/project/SampleJavaProject.java
+++ src/sample/java/project/SampleJavaProject.java
@@ -1,5 +1,11 @@
 package sample.java.project;
 
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
+
 /**
  * The main class.
  *
@@ -7,6 +13,9 @@ package sample.java.project;
  * method, the first method called.
  */
 public class SampleJavaProject {
+
+    /** The name to printed in the output message. */
+    private static String name = "world";
 
     /**
      * The main class.
@@ -16,7 +25,30 @@ public class SampleJavaProject {
      * @param args application input arguments
      */
     public static void main(final String[] args) {
-        System.out.println("Hello, world!");
+        /* Set up the command line arguments. */
+        Options options = new Options();
+        options.addOption(new Option("name", true, "set the user's name"));
+        options.addOption(new Option("help", "print this help message"));
+        CommandLine line = null;
+        try {
+            line = new GnuParser().parse(options, args);
+        } catch (org.apache.commons.cli.ParseException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+
+        /* Handle each argument. */
+        if (line.hasOption("help")) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("SampleJavaProject [options]", options);
+            System.exit(0);
+        }
+        if (line.hasOption("name")) {
+            name = line.getOptionValue("name");
+        }
+
+        /* Print out our message. */
+        System.out.printf("Hello, %s!\n", name);
     }
 
     /**
