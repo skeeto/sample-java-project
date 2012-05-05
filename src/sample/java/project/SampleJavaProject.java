2b306a4cc890b5c19216c1a28e82c0102573cd7d
Christopher Wellons <mosquitopsu@gmail.com>
2012-05-04 23:27:33 -0400

    Switch to JCommander argument parsing.

--- src/sample/java/project/SampleJavaProject.java
+++ src/sample/java/project/SampleJavaProject.java
@@ -1,17 +1,13 @@
 package sample.java.project;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.Setter;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.GnuParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
 
 /**
  * The main class of the application. It contains the main() method,
@@ -19,54 +15,56 @@ import org.apache.commons.cli.Options;
  */
 @NoArgsConstructor
 @AllArgsConstructor
-public class SampleJavaProject extends TimerTask {
+public class SampleJavaProject implements Runnable {
 
     /** The delay between printed messages. */
     private static final long PRINT_DELAY = 1000L;
 
     /** The name to be printed in the output message. */
     @Getter @Setter @NonNull
+    @Parameter(names = "--name", description = "set the user's name",
+               required = true)
     private String name = "world";
+
+    /** Command line parameter for --loop. */
+    @Parameter(names = "--loop", description = "print endlessly, hotswap demo")
+    private boolean loop = false;
+
+    /** Command line parameter for --help. */
+    @Parameter(names = { "-h", "--help" }, description = "print help message")
+    private boolean help = false;
 
     /**
      * Print the "Hello, world!" string.
      * @param args application input arguments
      */
     public static void main(final String[] args) {
-        /* Set up the command line arguments. */
-        Options options = new Options();
-        options.addOption(new Option("name", true, "set the user's name"));
-        options.addOption(new Option("loop", "print endlessly, hotswap demo"));
-        options.addOption(new Option("help", "print this help message"));
-        CommandLine line = null;
+        /* Parse command line arguments. */
+        SampleJavaProject sjp = new SampleJavaProject();
         try {
-            line = new GnuParser().parse(options, args);
-        } catch (org.apache.commons.cli.ParseException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
+            JCommander jc = new JCommander(sjp, args);
+            if (sjp.help) {
+                jc.usage();
+                return;
+            }
+        } catch (ParameterException e) {
+            System.err.println("error: " + e.getMessage());
+            new JCommander(new SampleJavaProject()).usage();
+            System.exit(-1);
         }
 
-        /* Handle each argument. */
-        SampleJavaProject sjp;
-        if (line.hasOption("help")) {
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("SampleJavaProject [options]", options);
-            System.exit(0);
-        }
-        if (line.hasOption("name")) {
-            sjp = new SampleJavaProject(line.getOptionValue("name"));
-        } else {
-            sjp = new SampleJavaProject();
-        }
-        if (line.hasOption("loop")) {
-            new Timer().schedule(sjp, 0L, PRINT_DELAY);
-        } else {
-            sjp.run();
-        }
+        sjp.run();
     }
 
     @Override
     public final void run() {
-        System.out.printf("Hello, %s!\n", name);
+        do {
+            System.out.printf("Hello, %s!%n", name);
+            try {
+                Thread.sleep(PRINT_DELAY);
+            } catch (InterruptedException e) {
+                return;
+            }
+        } while (loop);
     }
 }
