1f633b4007019d028f1d16b48f6a333e392bb7e7
Christopher Wellons <mosquitopsu@gmail.com>
2013-07-17 11:23:10 -0400

    Split off sayHello() method for hotswap demo.

--- src/sample/java/project/SampleJavaProject.java
+++ src/sample/java/project/SampleJavaProject.java
@@ -56,10 +56,17 @@ public class SampleJavaProject implements Runnable {
         sjp.run();
     }
 
+    /**
+     * Print the "Hello, world!" string.
+     */
+    public final void sayHello() {
+        System.out.printf("Hello, %s!%n", name);
+    }
+
     @Override
     public final void run() {
         do {
-            System.out.printf("Hello, %s!%n", name);
+            sayHello();
             try {
                 Thread.sleep(PRINT_DELAY);
             } catch (InterruptedException e) {
